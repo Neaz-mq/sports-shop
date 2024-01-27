bace63ac56b6e7719dbfb9278448b378b052c9bf
Neaz-mq <neazmorshed666@gmail.com>
2024-01-27 11:43:18 +0600

    working with swipper

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import NavBar from "../pages/Shared/NavBar/NavBar";
-import Banner from "../pages/Home/Banner/Banner";
+
 
 
 const Main = () => {
