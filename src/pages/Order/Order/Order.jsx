41f00c43cad2091c3691080854d08c4ce28dca1b
Neaz-mq <neazmorshed666@gmail.com>
2024-02-11 12:52:35 +0600

    Items of cricket add view all button

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -15,60 +15,59 @@ const Order = () => {
   const football = item.filter((item) => item.category === 'football');
   const tennis = item.filter((item) => item.category === 'tennis');
   const volleyball = item.filter((item) => item.category === 'volleyball');
-  const offered = item.filter((item) => item.category === 'offered');
 
   return (
     <div>
       <Cover img={orderCover} title={'Order Item'}></Cover>
 
       <div className="flex justify-center mt-8 ">
-        <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}> 
+        <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
 
-        
-     <TabList className="flex bg-gray-200 p-4 gap-6 ml-4 mb-8 mr-4" >
-     
-  <Tab className="py-2 px-4 ml-56 cursor-pointer text-gray-600 hover:text-blue-500 focus:outline-none border-b-2 border-transparent hover:border-blue-500">
-    Cricket
-  </Tab>
- 
-  
- 
-  <Tab className="py-2 px-4 cursor-pointer text-gray-600 hover:text-blue-500 focus:outline-none border-b-2 border-transparent hover:border-blue-500">
-    Football
-  </Tab>
- 
 
- 
-  <Tab className="py-2 px-4 cursor-pointer text-gray-600 hover:text-blue-500 focus:outline-none border-b-2 border-transparent hover:border-blue-500">
-    Tennis
-  </Tab>
- 
- 
-  <Tab className="py-2 px-4 cursor-pointer text-gray-600 hover:text-blue-500 focus:outline-none border-b-2 border-transparent hover:border-blue-500">
-    Volleyball
-  </Tab>
- 
-</TabList>
+          <TabList className="flex bg-gray-200 p-4 gap-6 ml-4 mb-8 mr-4" >
+
+            <Tab className="py-2 px-4 ml-56 cursor-pointer text-gray-600 hover:text-blue-500 focus:outline-none border-b-2 border-transparent hover:border-blue-500">
+              Cricket
+            </Tab>
+
+
+
+            <Tab className="py-2 px-4 cursor-pointer text-gray-600 hover:text-blue-500 focus:outline-none border-b-2 border-transparent hover:border-blue-500">
+              Football
+            </Tab>
+
+
+
+            <Tab className="py-2 px-4 cursor-pointer text-gray-600 hover:text-blue-500 focus:outline-none border-b-2 border-transparent hover:border-blue-500">
+              Tennis
+            </Tab>
+
+
+            <Tab className="py-2 px-4 cursor-pointer text-gray-600 hover:text-blue-500 focus:outline-none border-b-2 border-transparent hover:border-blue-500">
+              Volleyball
+            </Tab>
+
+          </TabList>
 
 
           <div className="p-4 mb-10">
             <TabPanel>
-             <OrderTab items={cricket}></OrderTab>
+              <OrderTab items={cricket}></OrderTab>
             </TabPanel>
             <TabPanel>
-            
-            <OrderTab items={football}></OrderTab>
-              
+
+              <OrderTab items={football}></OrderTab>
+
             </TabPanel>
             <TabPanel>
-           
-            <OrderTab items={tennis}></OrderTab>
-             
+
+              <OrderTab items={tennis}></OrderTab>
+
             </TabPanel>
             <TabPanel>
-           
-            <OrderTab items={volleyball}></OrderTab>
-              
+
+              <OrderTab items={volleyball}></OrderTab>
+
             </TabPanel>
           </div>
         </Tabs>
--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,13 +1,39 @@
-import ItemCard from "../../../components/ItemCard/ItemCard";
+import React, { useState } from 'react';
+import ItemCard from '../../../components/ItemCard/ItemCard';
 
-const OrderTab = ({items}) => {
-    return (
-        <div className='grid md:grid-cols-2 gap-16'>
-              {items.map((item) => (
-                <ItemCard key={item._id} item={item} />
-              ))}
-              </div>
-    );
+const OrderTab = ({ items }) => {
+  const [showAll, setShowAll] = useState(false);
+
+  // Determine the number of items to display initially
+  const displayedItems = showAll ? items : items.slice(0, 4);
+
+  return (
+    <div className='grid md:grid-cols-2 gap-16'>
+      {displayedItems.map((item) => (
+        <ItemCard key={item._id} item={item} />
+      ))}
+      {!showAll && items.length > 4 && (
+        <div className='flex justify-center items-center mt-4 ml-64 -mr-52'>
+          <button
+            className='btn btn-secondary'
+            onClick={() => setShowAll(true)}
+          >
+            View All
+          </button>
+        </div>
+      )}
+      {showAll && (
+        <div className='flex justify-center items-center mt-4 ml-64 -mr-52'>
+          <button
+            className='btn btn-secondary'
+            onClick={() => setShowAll(false)}
+          >
+            View Less
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default OrderTab;
