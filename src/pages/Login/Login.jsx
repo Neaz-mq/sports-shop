182a20f233b5da556cdfcba0bb63cd70bca97430
Neaz-mq <neazmorshed666@gmail.com>
2024-02-15 13:30:15 +0600

    firebase install

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 
 const Login = () => {
+
+    const {signIn} = useContext(AuthContext);
+
+
 
     const handleLogin = event => {
         event.preventDefault();
@@ -9,6 +15,11 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
+        signIn(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
     }
 
 
