77b7fc82350f7c92c70fe62997415573a1f676bf
Neaz-mq <neazmorshed666@gmail.com>
2024-02-27 10:58:15 +0600

    admin role work

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,16 +1,64 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const {data: users =[]} = useQuery({
+    const {data: users =[], refetch} = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+
+    const handleMakeAdmin = user =>{
+        axiosSecure.patch(`/users/admin/${user._id}`)
+        .then(res =>{
+            console.log(res.data)
+            if(res.data.modifiedCount > 0){
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        })
+    }
+
+
+
+    const handleDeleteUser = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
+
+
     return (
         <div>
         <div className="flex justify-evenly my-4">
@@ -22,7 +70,7 @@ const AllUsers = () => {
                 {/* head */}
                 <thead>
                     <tr>
-                        <th></th>
+                        <th>No.</th>
                         <th>Name</th>
                         <th>Email</th>
                         <th>Role</th>
@@ -37,15 +85,15 @@ const AllUsers = () => {
                             <td>{user.email}</td>
                             <td>
                             { user.role === 'admin' ? 'Admin' : <button
-                                    onClick={() => handleMakeAdmin(user)}
-                                    className="btn btn-lg bg-orange-500">
-                                    <FaUsers className="text-white 
-                                    text-2xl"></FaUsers>
-                                </button>}
+                                        onClick={() => handleMakeAdmin(user)}
+                                        className="btn btn-lg bg-orange-500">
+                                        <FaUsers className="text-white 
+                                        text-2xl"></FaUsers>
+                                    </button>}
                             </td>
                             <td>
                                 <button
-                                    onClick={() => handleDeleteUser(user)}
+                                     onClick={() => handleDeleteUser(user)}
                                     className="btn btn-ghost btn-lg">
                                     <FaTrashAlt className="text-red-600"></FaTrashAlt>
                                 </button>
