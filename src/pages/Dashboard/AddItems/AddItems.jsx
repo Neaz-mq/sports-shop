f0a006602c9e7bc35c6f079ad1a925a0e522d99e
Neaz-mq <neazmorshed666@gmail.com>
2024-03-07 11:47:26 +0600

    add item form work

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -1,24 +1,35 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 const AddItems = () => {
-    const { register, handleSubmit} = useForm();
-    const onSubmit = (data) => 
-        {
-            console.log(data)  
-        };
+    const { register, handleSubmit } = useForm();
+    const axiosPublic = useAxiosPublic();
+    const onSubmit = async (data) => {
+        console.log(data)
+        // image upload to imgbb and then get an url
+        const imageFile = { image: data.image[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        });
+        console.log(res.data);
+    };
     return (
         <div>
-             <SectionTitle heading="Add an item" subHeading="What's new?" ></SectionTitle>
-             <div>
-           <form onSubmit={handleSubmit(onSubmit)}>
+            <SectionTitle heading="Add an item" subHeading="What's new?" ></SectionTitle>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-control w-full my-6">
                         <label className="label">
-                            <span className="label-text">Recipe Name*</span>
+                            <span className="label-text">Item Name*</span>
                         </label>
                         <input
                             type="text"
-                            placeholder="Recipe Name"
+                            placeholder="Item Name"
                             {...register('name', { required: true })}
                             required
                             className="input input-bordered w-full" />
@@ -36,7 +47,7 @@ const AddItems = () => {
                                 <option value="football">Football</option>
                                 <option value="tennis">Tennis</option>
                                 <option value="volleyball">Volleyball</option>
-                               
+
                             </select>
                         </div>
 
@@ -53,12 +64,13 @@ const AddItems = () => {
                         </div>
 
                     </div>
-                    {/* recipe details */}
+                    {/* Item details */}
+                  
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Recipe Details</span>
+                            <span className="label-text">Item Details</span>
                         </label>
-                        <textarea {...register('recipe')} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+                        <textarea {...register('description')} className="textarea textarea-bordered h-24" placeholder="Details"></textarea>
                     </div>
 
                     <div className="form-control w-full my-6">
@@ -66,10 +78,10 @@ const AddItems = () => {
                     </div>
 
                     <button className="btn btn-accent">
-                        Add Item 
+                        Add Item
                     </button>
                 </form>
-           </div>
+            </div>
         </div>
     );
 };
